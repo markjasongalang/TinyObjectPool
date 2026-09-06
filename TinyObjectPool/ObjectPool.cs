49827ee916223a71957d315619dd9d024f575aec
markjasongalang <markjasongalang3@gmail.com>
2026-09-06 12:12:22 +0800

    feat: add initial object pool implementation

---
+++ TinyObjectPool/ObjectPool.cs
@@ -0,0 +1,26 @@
+﻿namespace TinyObjectPool;
+
+public class ObjectPool<T> where T : class, new() // Compiler needs to know 'T' has a parameterless constructor
+{
+    private readonly Stack<T> _objectPool;
+
+    public ObjectPool(int maxSize)
+    {
+        _objectPool = new Stack<T>(maxSize);
+
+        for (var i = 0; i < maxSize; i++)
+        {
+            _objectPool.Push(new T());
+        }
+    }
+
+    public T? Rent()
+    {
+        return _objectPool.Pop();
+    }
+
+    public void Return(T item)
+    {
+        _objectPool.Push(item);
+    }
+}
--- TinyObjectPool/Program.cs
+++ TinyObjectPool/Program.cs
@@ -1,7 +1,25 @@
-﻿public class Program
+﻿using TinyObjectPool;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var pool = new ObjectPool<MyObject>(5);
+
+        MyObject? a = pool.Rent();
+        MyObject? b = pool.Rent();
+
+        pool.Return(a);
+
+        MyObject? c = pool.Rent();
+
+        // Reference identity check (same object in memory)
+        bool isSameInstance = ReferenceEquals(a, c);
+        Console.WriteLine($"Is exact same object? {isSameInstance}");
     }
+}
+
+public class MyObject
+{
+    // Empty for now
 }
